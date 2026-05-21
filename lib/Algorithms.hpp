bfa51682976514db6bab16e660a785b008d12c8c
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-21 09:34:33 +0700

    Add linearSearch, binarySearch

--- lib/Algorithms.hpp
+++ lib/Algorithms.hpp
@@ -1,9 +1,52 @@
 #ifndef ALGORITHMS_HPP
 #define ALGORITHMS_HPP
 
-#include <functional> // Chứa std::less<T> mặc định
+#include <functional> // Chứa std::less<T> mặc định.
 
-// --- CÁC THUẬT TOÁN SẮP XẾP ---
+/* --- CÁC THUẬT TOÁN TÌM KIẾM --- */
+
+// Linear search.
+template <typename T>
+int linearSearch(T arr[], int left, int right, const T &value) // Range: [left, right].
+{
+    for (int i = left; i <= right; i++)
+    {
+        if (arr[i] == value)
+            return i;
+    }
+    return -1;
+}
+
+template <typename T>
+int linearSearch(T arr[], int sizeArr, const T &value)
+{
+    return linearSearch(arr, 0, sizeArr - 1, value);
+}
+
+// Binary search.
+template <typename T, typename Comp = std::less<T>>
+int binarySearch(T arr[], int left, int right, const T &value, Comp cmp = Comp()) // Range: [left, right].
+{
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value)
+            return mid;
+        else if (cmp(arr[mid], value))
+            left = mid + 1;
+        else // cmp(value, arr[mid])
+            right = mid - 1;
+    }
+    return -1;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int binarySearch(T arr[], int sizeArr, const T &value, Comp cmp = Comp())
+{
+    return binarySearch(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* --- CÁC THUẬT TOÁN SẮP XẾP --- */
 template <typename T, typename Comp = std::less<T>>
 void bubbleSort(T arr[], int n, Comp cmp = Comp());
 
@@ -21,12 +64,5 @@ void quickSort(T arr[], int lo, int hi, Comp cmp = Comp());
 
 template <typename T, typename Comp = std::less<T>>
 void mergeSort(T arr[], int n, Comp cmp = Comp());
-
-// --- CÁC THUẬT TOÁN TÌM KIẾM ---
-template <typename T>
-int linearSearch(T arr[], int n, const T& key);
-
-template <typename T>
-int binarySearch(T arr[], int n, const T& key); // Yêu cầu arr[] đã được sắp xếp
 
 #endif // ALGORITHMS_HPP
