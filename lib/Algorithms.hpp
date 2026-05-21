2a18935902fd6bf61408f1e38c1e573691bd4157
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-21 13:27:34 +0700

    Add lowerbound, upperbound.

--- lib/Algorithms.hpp
+++ lib/Algorithms.hpp
@@ -10,7 +10,7 @@
 - Range: [left, right].
 - Time complexity: O(n).
 - Space complexity: O(1).
-- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the first occurrence of value in array (if found); otherwise, return -1. */
 
 template <typename T>
 int linearSearch(T arr[], int left, int right, const T &value)
@@ -36,7 +36,7 @@ int linearSearch(T arr[], int sizeArr, const T &value)
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1).
-- Return: index of value in array (if found), otherwise return -1. */
+- Return: index of value in array (if found); otherwise, return -1. */
 
 template <typename T, typename Comp = std::less<T>>
 int binarySearch(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
@@ -67,7 +67,7 @@ int binarySearch(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1).
-- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the first occurrence of value in array (if found); otherwise, return -1. */
 
 template <typename T, typename Comp = std::less<T>>
 int binarySearchFirst(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
@@ -78,7 +78,7 @@ int binarySearchFirst(T arr[], int left, int right, const T &value, const Comp &
         int mid = left + (right - left) / 2;
         if (arr[mid] == value)
         {
-            index = mid;     // lưu vị trí tìm thấy.
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
             right = mid - 1; // tiếp tục tìm bên trái.
         }
         else if (cmp(arr[mid], value))
@@ -102,7 +102,7 @@ int binarySearchFirst(T arr[], int sizeArr, const T &value, const Comp &cmp = Co
 - Range: [left, right].
 - Time complexity: O(log n).
 - Space complexity: O(1).
-- Return: index of the last occurrence of value in array (if found), otherwise return -1. */
+- Return: index of the last occurrence of value in array (if found); otherwise, return -1. */
 
 template <typename T, typename Comp = std::less<T>>
 int binarySearchLast(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
@@ -113,7 +113,7 @@ int binarySearchLast(T arr[], int left, int right, const T &value, const Comp &c
         int mid = left + (right - left) / 2;
         if (arr[mid] == value)
         {
-            index = mid;    // lưu vị trí tìm thấy.
+            index = mid;    // lưu vị trí hợp lệ hiện tại.
             left = mid + 1; // tiếp tục tìm bên phải.
         }
         else if (cmp(arr[mid], value))
@@ -128,6 +128,78 @@ template <typename T, typename Comp = std::less<T>>
 int binarySearchLast(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
 {
     return binarySearchLast(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Lower_bound(value):
+- Vietnamese: Tìm kiếm nhị phân vị trí đầu tiên KHÔNG đứng trước value,
+tức là: "key !< value" <=> "key >= value".
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return:
++ Index of the first element that is NOT less than value according to comparator (if found).
++ Otherwise, return right + 1. */
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBound(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = right + 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (!cmp(arr[mid], value))
+        {
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
+            right = mid - 1; // tiếp tục tìm bên trái.
+        }
+        else // cmp(arr[mid], value)
+            left = mid + 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int lowerBound(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return lowerBound(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Upper_bound(value):
+- Vietnamese: Tìm kiếm nhị phân vị trí đầu tiên value đứng trước nó,
+tức là: "value < key" <=> "key > value".
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return:
++ Index of the first element greater than value according to comparator (if found).
++ Otherwise, return right + 1. */
+
+template <typename T, typename Comp = std::less<T>>
+int upperBound(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = right + 1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (cmp(value, arr[mid]))
+        {
+            index = mid;     // lưu vị trí hợp lệ hiện tại.
+            right = mid - 1; // tiếp tục tìm bên trái.
+        }
+        else // !cmp(value, arr[mid])
+            left = mid + 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int upperBound(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return upperBound(arr, 0, sizeArr - 1, value, cmp);
 }
 
 /* --- CÁC THUẬT TOÁN SẮP XẾP --- */
