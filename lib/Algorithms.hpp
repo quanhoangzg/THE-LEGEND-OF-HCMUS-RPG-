04e88519fd655693387f0fe397547ec4a0cc96e4
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-21 10:18:08 +0700

    Add linearSearch, binarySearch, binarySearchFirst, binarySearchLast.

--- lib/Algorithms.hpp
+++ lib/Algorithms.hpp
@@ -5,9 +5,15 @@
 
 /* --- CÁC THUẬT TOÁN TÌM KIẾM --- */
 
-// Linear search.
+/* Linear search (Sequential search):
+- Vietnamese: Tìm kiếm tuyến tính (Tìm kiếm tuần tự).
+- Range: [left, right].
+- Time complexity: O(n).
+- Space complexity: O(1).
+- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+
 template <typename T>
-int linearSearch(T arr[], int left, int right, const T &value) // Range: [left, right].
+int linearSearch(T arr[], int left, int right, const T &value)
 {
     for (int i = left; i <= right; i++)
     {
@@ -23,9 +29,17 @@ int linearSearch(T arr[], int sizeArr, const T &value)
     return linearSearch(arr, 0, sizeArr - 1, value);
 }
 
-// Binary search.
+/* Binary search:
+- Vietnamese: Tìm kiếm nhị phân.
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return: index of value in array (if found), otherwise return -1. */
+
 template <typename T, typename Comp = std::less<T>>
-int binarySearch(T arr[], int left, int right, const T &value, Comp cmp = Comp()) // Range: [left, right].
+int binarySearch(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
 {
     while (left <= right)
     {
@@ -41,9 +55,79 @@ int binarySearch(T arr[], int left, int right, const T &value, Comp cmp = Comp()
 }
 
 template <typename T, typename Comp = std::less<T>>
-int binarySearch(T arr[], int sizeArr, const T &value, Comp cmp = Comp())
+int binarySearch(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
 {
     return binarySearch(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Binary search first occurrence:
+- Vietnamese: Tìm kiếm nhị phân phần tử xuất hiện đầu tiên.
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return: index of the first occurrence of value in array (if found), otherwise return -1. */
+
+template <typename T, typename Comp = std::less<T>>
+int binarySearchFirst(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value)
+        {
+            index = mid;     // lưu vị trí tìm thấy.
+            right = mid - 1; // tiếp tục tìm bên trái.
+        }
+        else if (cmp(arr[mid], value))
+            left = mid + 1;
+        else // cmp(value, arr[mid])
+            right = mid - 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int binarySearchFirst(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return binarySearchFirst(arr, 0, sizeArr - 1, value, cmp);
+}
+
+/* Binary search last occurrence:
+- Vietnamese: Tìm kiếm nhị phân phần tử xuất hiện cuối cùng.
+- Condition: array must be sorted according to comparator
+(mảng được sắp xếp, hay mảng là một dãy đơn điệu theo comparator).
+- Range: [left, right].
+- Time complexity: O(log n).
+- Space complexity: O(1).
+- Return: index of the last occurrence of value in array (if found), otherwise return -1. */
+
+template <typename T, typename Comp = std::less<T>>
+int binarySearchLast(T arr[], int left, int right, const T &value, const Comp &cmp = Comp())
+{
+    int index = -1;
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == value)
+        {
+            index = mid;    // lưu vị trí tìm thấy.
+            left = mid + 1; // tiếp tục tìm bên phải.
+        }
+        else if (cmp(arr[mid], value))
+            left = mid + 1;
+        else // cmp(value, arr[mid])
+            right = mid - 1;
+    }
+    return index;
+}
+
+template <typename T, typename Comp = std::less<T>>
+int binarySearchLast(T arr[], int sizeArr, const T &value, const Comp &cmp = Comp())
+{
+    return binarySearchLast(arr, 0, sizeArr - 1, value, cmp);
 }
 
 /* --- CÁC THUẬT TOÁN SẮP XẾP --- */
