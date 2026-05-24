003f91a5d7f7d899c9b9b1fba80373d3bdd7ee7e
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-24 18:17:51 +0700

    Finished PriorityQueue.hpp

--- lib/PriorityQueue.hpp
+++ lib/PriorityQueue.hpp
@@ -1,20 +1,98 @@
 #ifndef PRIORITY_QUEUE_HPP
 #define PRIORITY_QUEUE_HPP
 
+#include <vector>
+#include <functional>
+
 template <typename T>
-struct PriorityQueue {
-    T* heap = nullptr;
-    int count = 0;
-    int capacity = 0;
+void mySwap(T &a, T &b)
+{
+    T temp = a;
+    a = b;
+    b = temp;
+}
+
+template <typename T>
+struct PriorityQueue
+{
+    vector<T> heap;
 };
 
-// --- Các hàm cốt lõi ---
-template <typename T> void insert(PriorityQueue<T>& pq, const T& item);
-template <typename T> T extract(PriorityQueue<T>& pq);
-template <typename T> T peek(const PriorityQueue<T>& pq);
-template <typename T> bool empty(const PriorityQueue<T>& pq);
-template <typename T> int size(const PriorityQueue<T>& pq);
-template <typename T> void initPQ(PriorityQueue<T>& pq, int cap);
-template <typename T> void clear(PriorityQueue<T>& pq);
+template <typename T>
+void init(PriorityQueue<T> &pq)
+{
+    pq.heap.clear();
+}
+
+template <typename T>
+bool isEmpty(const PriorityQueue<T> &pq)
+{
+    return pq.heap.empty();
+}
+
+template <typename T>
+int getSize(const PriorityQueue<T> &pq)
+{
+    return pq.heap.size();
+}
+
+template <typename T>
+const T &peek(const PriorityQueue<T> &pq)
+{
+    return pq.heap[0];
+}
+
+template <typename T, typename Comp = std::less<T>>
+void heapifyUp(PriorityQueue<T> &pq, int index, const Comp &cmp = Comp())
+{
+    while (index > 0)
+    {
+        int parent = (index - 1) / 2;
+        if (cmp(pq.heap[parent], pq.heap[index]))
+        {
+            mySwap(pq.heap[index], pq.heap[parent]);
+            index = parent;
+        }
+        else
+            break;
+    }
+}
+
+template <typename T, typename Comp = std::less<T>>
+void heapifyDown(PriorityQueue<T> &pq, int index, const Comp &cmp = Comp())
+{
+    int sizeHeap = pq.heap.size();
+    while (2 * index + 1 < sizeHeap)
+    {
+        int best = index, left = 2 * index + 1, right = 2 * index + 2;
+        if (left < sizeHeap && cmp(pq.heap[best], pq.heap[left]))
+            best = left;
+        if (right < sizeHeap && cmp(pq.heap[best], pq.heap[right]))
+            best = right;
+        if (best == index)
+            break;
+        mySwap(pq.heap[index], pq.heap[best]);
+        index = best;
+    }
+}
+
+template <typename T, typename Comp = std::less<T>>
+void push(PriorityQueue<T> &pq, const T &value, const Comp &cmp = Comp())
+{
+    pq.heap.push_back(value);
+    int sizeHeap = pq.heap.size();
+    heapifyUp(pq, sizeHeap - 1, cmp);
+}
+
+template <typename T, typename Comp = std::less<T>>
+void pop(PriorityQueue<T> &pq, const Comp &cmp = Comp())
+{
+    if (pq.heap.empty())
+        return;
+    mySwap(pq.heap[0], pq.heap.back());
+    pq.heap.pop_back();
+    if (!pq.heap.empty())
+        heapifyDown(pq, 0, cmp);
+}
 
 #endif // PRIORITY_QUEUE_HPP
