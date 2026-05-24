e8fc135ba50372c4073eb3ad7e516e9428b6a112
Van Thanh Phat <vanthanhphat19110@gmail.com>
2026-05-24 18:16:06 +0700

    Finished Queue.hpp

--- lib/Queue.hpp
+++ lib/Queue.hpp
@@ -2,21 +2,88 @@
 #define QUEUE_HPP
 
 template <typename T>
-struct Queue {
-    T* data = nullptr;
-    int frontIndex = 0;
-    int rearIndex = -1;
-    int count = 0;
-    int capacity = 0;
+struct QueueNode
+{
+    T data;
+    QueueNode<T> *next;
+    QueueNode(const T &value) : data(value), next(nullptr) {};
 };
 
-// --- Các hàm cốt lõi ---
-template <typename T> void enqueue(Queue<T>& q, const T& item);
-template <typename T> T dequeue(Queue<T>& q);
-template <typename T> T front(const Queue<T>& q);
-template <typename T> bool empty(const Queue<T>& q);
-template <typename T> int size(const Queue<T>& q);
-template <typename T> void initQueue(Queue<T>& q, int cap);
-template <typename T> void clear(Queue<T>& q);
+template <typename T>
+struct Queue
+{
+    QueueNode<T> *front;
+    QueueNode<T> *back;
+    int size;
+};
+
+template <typename T>
+void init(Queue<T> &q)
+{
+    q.front = nullptr;
+    q.back = nullptr;
+    q.size = 0;
+}
+
+template <typename T>
+bool isEmpty(const Queue<T> &q)
+{
+    return q.front == nullptr;
+}
+
+template <typename T>
+int getSize(const Queue<T> &q)
+{
+    return q.size;
+}
+
+template <typename T>
+const T &getFront(const Queue<T> &q)
+{
+    return q.front->data;
+}
+
+template <typename T>
+void enqueue(Queue<T> &q, const T &value)
+{
+    QueueNode<T> *p = new QueueNode<T>(value);
+    if (q.front == nullptr)
+    {
+        q.front = p;
+        q.back = p;
+    }
+    else // q.front != nullptr
+    {
+        q.back->next = p;
+        q.back = p;
+    }
+    q.size++;
+}
+
+template <typename T>
+void dequeue(Queue<T> &q)
+{
+    if (q.front == nullptr)
+        return;
+    QueueNode<T> *temp = q.front;
+    q.front = q.front->next;
+    delete temp;
+    q.size--;
+    if (q.front == nullptr)
+        q.back = nullptr;
+}
+
+template <typename T>
+void clear(Queue<T> &q)
+{
+    while (q.front != nullptr)
+    {
+        QueueNode<T> *temp = q.front;
+        q.front = q.front->next;
+        delete temp;
+    }
+    q.back = nullptr;
+    q.size = 0;
+}
 
 #endif // QUEUE_HPP
